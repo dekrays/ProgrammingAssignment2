eebd960ae7272a59241a3b10f971e7fbcb77f64a
dekrays <dekrays@yandex.ru>
2014-06-21 21:25:21 +0200

    Test Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Johns Hopkins University
+## Data Science
+## A Specialization on Coursera
 
-## Write a short comment describing this function
+## R Programming
+## Programming Assignment 2: Lexical Scoping
 
+## Implementation of two functions adding caching capability to matrix type
+
+## Makes a wrapper object for a matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) i <<- inv
+    getinv <- function() i
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Wrapper for solve() function adding caching
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinv()
+    if (!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinv(i)
+    ## Return a matrix that is the inverse of 'x'
+    i
 }
